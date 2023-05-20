ff68971d87efc912a4ca24d9127390969af9d074
repoGit20_5 <aegnora123@gmail.com>
2023-05-20 13:20:07 +0000

    wiek

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:")
-# Sprawdzamy czy podany wiek jest liczbą
-if wiek.isdigit() == False:
-	exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
-wiek=int(wiek)
-if wiek>=18 and wiek<40:
-	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-elif wiek>=40:
-	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-	print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
-else:
-  exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
+wiek=int(input('podaj wiek: '))
+
+if wiek>=18:
+
+   print("Jestes pelnoletni")
+
+if wiek<18:
+
+   print("Nie jestes pelnoletni")
